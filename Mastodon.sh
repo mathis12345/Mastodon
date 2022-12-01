7442624f55e56c5175a676d470dd966dd98e590b
mathis12345 <116630424+mathis12345@users.noreply.github.com>
2022-12-01 12:58:48 +0100

    Update Mastodon.sh

--- Mastodon.sh
+++ Mastodon.sh
@@ -29,8 +29,6 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 
-exec bash
-
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 RUBY_CONFIGURE_OPTS=--with-jemalloc rbenv install 3.0.4
@@ -61,7 +59,6 @@ git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
 bundle config deployment 'true'
 
 bundle config without 'development test'
-
 bundle install -j$(getconf _NPROCESSORS_ONLN)
 
 yarn install --pure-lockfile
