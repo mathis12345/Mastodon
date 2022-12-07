15f5c6cd09ec754d3a138ff7169a25e66457d01d
mathis12345 <116630424+mathis12345@users.noreply.github.com>
2022-12-07 14:53:01 +0100

    Update M.sh

--- M.sh
+++ M.sh
@@ -1,3 +1,15 @@
+cp /home/mastodon/live/dist/mastodon-web.service /etc/systemd/system/
+cp /home/mastodon/live/dist/mastodon-sidekiq.service /etc/systemd/system/
+cp /home/mastodon/live/dist/mastodon-streaming.service /etc/systemd/system/
+
+
+
+
+
+
+
+
+
 systemctl start mastodon-web
 systemctl start mastodon-sidekiq
 systemctl start mastodon-streaming
