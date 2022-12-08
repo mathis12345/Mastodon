10fdd53a6394adbd4c6eefc50eba1556796db526
mathis12345 <116630424+mathis12345@users.noreply.github.com>
2022-12-08 09:16:06 +0100

    Update Mastodon_003.sh

--- Mastodon_003.sh
+++ Mastodon_003.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+cp /home/mastodon/live/dist/mastodon-web.service /etc/systemd/system/
+
+cp /home/mastodon/live/dist/mastodon-sidekiq.service /etc/systemd/system/
+
+cp /home/mastodon/live/dist/mastodon-streaming.service /etc/systemd/system/
+
+systemctl start mastodon-web
+
+systemctl start mastodon-sidekiq
+
+systemctl start mastodon-streaming
+
+systemctl enable mastodon-web
+
+systemctl enable mastodon-sidekiq
+
+systemctl enable mastodon-streaming
+
+systemctl status mastodon-web mastodon-sidekiq mastodon-streaming
